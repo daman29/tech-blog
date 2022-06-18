46abc3641478f3f61827d57667d05ed02db4f980
Damneet Sambhy <damneet.sambhy@hotmail.com>
2022-06-18 11:36:04 +0930

    post model created

---
+++ models/Post.js
@@ -0,0 +1,39 @@
+const {Model, DataTypes} = require('sequelize');
+
+class Post extends Model {}
+
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        post_title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        post_content: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        created_at: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW
+        }
+    },
+    {
+        sequelize,
+        modelName: 'post',
+        timestamps: false,
+        underscored: true,
+        freezeTableName: true
+    }
+);
+
+module.exports = Post;
