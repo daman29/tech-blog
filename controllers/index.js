05e54942d8490e80c3d15e122865664769ccde18
Damneet Sambhy <damneet.sambhy@hotmail.com>
2022-06-19 13:43:21 +0930

    added user api routes

--- controllers/index.js
+++ controllers/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 
 const postRoutes = require('./postRoutes');
 const homeRoutes = require('./homeRoutes');
+const apiRoutes = require('./api/index');
 
 
 router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
 router.use('/post', postRoutes);
 
 module.exports = router;
